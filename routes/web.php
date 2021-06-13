70ee1cc09e208b037d14d8fb964532bded81bf64
hau manh hung <hunghau55@gmail.com>
2021-06-13 16:59:17 +0700

    cut layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('web.post');
 });
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
